1c294f976003a7dad5e57666df91bb3ba3869e5f
Michael Wolf <52828804+Kalzec@users.noreply.github.com>
2019-10-04 13:06:18 +0100

    Add files via upload

---
+++ StateManager/Assets/Scripts/AttackState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : State {
+
+    int ammo;
+
+	// Use this for initialization
+	void Start () {
+        ammo = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (ammo <= 0)
+        {
+            Debug.Log("Out of ammo, changing state to jump...");
+            System.Threading.Thread.Sleep(1000);
+        }
+        else
+        {
+            Debug.Log("Shoot");
+            System.Threading.Thread.Sleep(350);
+            ammo--;
+        }
+
+	}
+}
